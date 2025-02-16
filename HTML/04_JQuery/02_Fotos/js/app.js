35e45eb02fb96a394997efc7ac43a59796d7b515
Rosariongc <i202331478@cibertec.edu.pe>
2025-02-16 18:34:11 +0000

    se trabaja con jquery

---
+++ HTML/04_JQuery/02_Fotos/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".mueble").hover(
+        function() {
+            $(this).css("transform", "scale(1.15)");
+        }, 
+        function(){
+            $(this).css("transform", "scale(1)");
+        }
+    )
+})
